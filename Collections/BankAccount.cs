1f408222ee3050c7db56a3a6f61154afd0514de6
GennadiiIurkevych <yourgensport@gmail.com>
2026-06-28 20:55:19 +0300

    start block4

--- Collections/BankAccount.cs
+++ Collections/BankAccount.cs
@@ -1,32 +1,34 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-//namespace Collections
-//{
-//    internal class BankAccount
-//    {
+namespace Collections
+{
+    internal class BankAccount
+    {
 
-//            private decimal _balance; //В цій стрічці зберігається стан рахунку. Щоб його не можна було якимось чином ззовні
-//                                      //змінювати на пряму тим самим запобігти маніпуляціям з рахунком
-//            public string Owner;     //Ця стрічка виводить на екран власника рахунку. Не знаю чи потрібна ця інформація щоб
-//                                      //була публічна?
-//            public decimal Balance => _balance; //В цій стрічці функція, що повертає баланс, вона тільки для читання даних
-//                                                //і зовнішній код не може змінити баланс напряму.
-//            public void Deposit(decimal amount) { _balance += amount; } //В цій стрічці користувач поповнює баланс контрольованим способом
-//        public void Withdraw(decimal amount)  //В цій стрічці користувач знімає кошти контрольованим способом
-//        { if (_balance <= 0)
-//            {
-//                Console.WriteLine("please top up the account");
-//            } else
-//            {
-//                _balance -= amount;
-//            }
-//        }
-//        private void RecalculateInterest() { /* внутрішня логіка */ }     /*Ця стрічка для якихось внутрішніх розрахунків і користувачу
-//                                                                            не потрібний доступ до неї напряму
-//                                                                            Тож, ми інкапсулюємо деякі методи і властивості 
-//                                                                            при яких зовнішній код (користувач) бачить*/ 
+        private decimal _balance; //В цій стрічці зберігається стан рахунку. Щоб його не можна було якимось чином ззовні
+                                  //змінювати на пряму тим самим запобігти маніпуляціям з рахунком
+        public string Owner;     //Ця стрічка виводить на екран власника рахунку. Не знаю чи потрібна ця інформація щоб
+                                 //була публічна?
+        public decimal Balance => _balance; //В цій стрічці функція, що повертає баланс, вона тільки для читання даних
+                                            //і зовнішній код не може змінити баланс напряму.
+        public void Deposit(decimal amount) { _balance += amount; } //В цій стрічці користувач поповнює баланс контрольованим способом
+        public void Withdraw(decimal amount)  //В цій стрічці користувач знімає кошти контрольованим способом
+        {
+            if (_balance <= 0)
+            {
+                Console.WriteLine("please top up the account");
+            }
+            else
+            {
+                _balance -= amount;
+            }
+        }
+        private void RecalculateInterest() { /* внутрішня логіка */ }     /*Ця стрічка для якихось внутрішніх розрахунків і користувачу
+                                                                            не потрібний доступ до неї напряму
+                                                                            Тож, ми інкапсулюємо деякі методи і властивості 
+                                                                            при яких зовнішній код (користувач) бачить*/
 
-//    }
-//}
+    }
+}
--- HelloCourse/Block0.cs
+++ HelloCourse/Block0.cs
@@ -1,127 +1,127 @@
-//﻿using System;
-//using System.Collections.Generic;
-//using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
-//namespace HelloCourse
-//{
-//    class Block0
-//    {
-//        static int Add(int number, int addend)
-//        {
-//            return number + addend;
-//        }
+namespace HelloCourse
+{
+    class Block0
+    {
+        static int Add(int number, int addend)
+        {
+            return number + addend;
+        }
 
-//        static string Greet(string Name)
-//        {
-//            return Name;
-//        }
+        static string Greet(string Name)
+        {
+            return Name;
+        }
 
-//        internal class Person
-//        {
-//            public string Name;
-//            public int Age;
+        internal class Person
+        {
+            public string Name;
+            public int Age;
 
-//            public Person(string name, int age)
-//            {
-//                Name = name;
-//                Age = age;
-//            }
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
 
-//            public void Introduce()
-//            {
-//                Console.WriteLine($"Hello, my name's {Name}, I'm {Age} years old");
-//            }
-//        }
+            public void Introduce()
+            {
+                Console.WriteLine($"Hello, my name's {Name}, I'm {Age} years old");
+            }
+        }
 
-//        static void Main()
-//        {
-//            //Завдання 1:
-//            int num = 8;
-//            Console.WriteLine(num);
+        static void Main()
+        {
+            //Завдання 1:
+            int num = 8;
+            Console.WriteLine(num);
 
-//            string word = "Welcome";
-//            Console.WriteLine(word);
+            string word = "Welcome";
+            Console.WriteLine(word);
 
-//            string name = "Alex";
-//            Console.WriteLine(name);
+            string name = "Alex";
+            Console.WriteLine(name);
 
-//            bool haveCat = false;
-//            Console.WriteLine(haveCat);
+            bool haveCat = false;
+            Console.WriteLine(haveCat);
 
-//            double numWithPoint = 12.56;
-//            Console.WriteLine(numWithPoint);
+            double numWithPoint = 12.56;
+            Console.WriteLine(numWithPoint);
 
-//            /*int number = null;  //Видається помилка, тому, що неможливо перетворити null у 'int' (або інший тип), оскільки це ненульований тип значення. 
-//                                  //У C# типи поділяються на типи посилань та типи значень. 
-//                                  //Типи посилань можуть вказувати на(порожньо / нічого).
-//                                  //Типи значень, однак, зберігають свої дані безпосередньо і завжди повинні мати дійсне значення.*/
+            /*int number = null;  //Видається помилка, тому, що неможливо перетворити null у 'int' (або інший тип), оскільки це ненульований тип значення. 
+                                  //У C# типи поділяються на типи посилань та типи значень. 
+                                  //Типи посилань можуть вказувати на(порожньо / нічого).
+                                  //Типи значень, однак, зберігають свої дані безпосередньо і завжди повинні мати дійсне значення.*/
 
-//            //Але, якщо ми поставимо знак ? після оголошення типу даних, то ми можемо присвоїти null:
+            //Але, якщо ми поставимо знак ? після оголошення типу даних, то ми можемо присвоїти null:
 
-//            int? number = null;
+            int? number = null;
 
-//            //Завдання 2:
+            //Завдання 2:
 
-//            //int userInput = Convert.ToInt32(Console.ReadLine());
-//            int userInput = int.Parse(Console.ReadLine()); //ще один метод перетворення стрічки в числовий тип даних
+            //int userInput = Convert.ToInt32(Console.ReadLine());
+            int userInput = int.Parse(Console.ReadLine()); //ще один метод перетворення стрічки в числовий тип даних
 
-//            Console.WriteLine("You input " + userInput);
+            Console.WriteLine("You input " + userInput);
 
-//            if (userInput % 2 == 0)
-//                Console.WriteLine("This is an even number");
-//            else
-//                Console.WriteLine("This is an odd number");
+            if (userInput % 2 == 0)
+                Console.WriteLine("This is an even number");
+            else
+                Console.WriteLine("This is an odd number");
 
-//            for (int i = 1; i <= userInput; i++)
-//            {
-//                Console.WriteLine(i);
-//            }
+            for (int i = 1; i <= userInput; i++)
+            {
+                Console.WriteLine(i);
+            }
 
-//            int iter = 1;
+            int iter = 1;
 
-//            while (iter <= userInput)
-//            {
-//                Console.WriteLine(iter);
-//                iter++;
-//            }
+            while (iter <= userInput)
+            {
+                Console.WriteLine(iter);
+                iter++;
+            }
 
-//            //Завдання 3:
-//            int sum = Add(4, 8);
-//            Console.WriteLine($"Total: {sum}");
+            //Завдання 3:
+            int sum = Add(4, 8);
+            Console.WriteLine($"Total: {sum}");
 
-//            string firstName = Greet("Gennadii");
-//            Console.WriteLine(firstName);
+            string firstName = Greet("Gennadii");
+            Console.WriteLine(firstName);
 
-//            //Завдання 4:
+            //Завдання 4:
 
-//            int[] numbers = { 4, 15, 7, 28, 3 };
+            int[] numbers = { 4, 15, 7, 28, 3 };
 
-//            int maxInt = 4;
+            int maxInt = 4;
 
-//            for (int i = 1; i < numbers.Length; i++)
+            for (int i = 1; i < numbers.Length; i++)
 
-//                if (maxInt < numbers[i])
+                if (maxInt < numbers[i])
 
-//                    maxInt = numbers[i];
+                    maxInt = numbers[i];
 
-//            Console.WriteLine(maxInt);
+            Console.WriteLine(maxInt);
 
-//            string[] List = { "John", "Alex", "July" };
+            string[] List = { "John", "Alex", "July" };
 
-//            foreach (string names in List)
+            foreach (string names in List)
 
-//                Console.WriteLine(names);
+                Console.WriteLine(names);
 
-//            //Завдання 5:
+            //Завдання 5:
 
-//            Person identPerson = new Person("Gennadii", 56);
-//            identPerson.Introduce();
+            Person identPerson = new Person("Gennadii", 56);
+            identPerson.Introduce();
 
-//            Person informPerson = new Person("Volodymyr", 24);
-//            Person anotherPerson = new Person("Max", 30);
-//            informPerson.Introduce();
-//            anotherPerson.Introduce();
+            Person informPerson = new Person("Volodymyr", 24);
+            Person anotherPerson = new Person("Max", 30);
+            informPerson.Introduce();
+            anotherPerson.Introduce();
 
-//        }
-//    }
-//}
+        }
+    }
+}
