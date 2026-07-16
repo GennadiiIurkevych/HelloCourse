4e87bdd3c3130cc17769da724c2da248db49551b
GennadiiIurkevych <yourgensport@gmail.com>
2026-07-16 11:37:20 +0300

    training2

---
+++ Interface/DefaultInterfaceImplementation.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface
+{
+    internal class DefaultInterfaceImplementation
+    {
+
+        public interface ILogger
+        {
+            //public ILogger()
+            //{
+            //    Console.WriteLine("ILogger constructor");
+            //}
+
+            //int a;
+
+            void Log(LogLevel logLevel, string message);
+
+            void Bar()
+            {
+                Console.WriteLine("Bar");
+            }
+
+            //int Sum(int a, int b)
+            //{
+            //    return a + b;
+            //}
+            void LogError(string message)
+            {
+                Log(LogLevel.Error, message);
+            }
+        }
+
+        public enum LogLevel
+        {
+            Debug,
+            Info,
+            Warning,
+            Error,
+            Fatal
+        }
+
+        public class ConsoleLogger : ILogger
+        {
+            public void Log(LogLevel logLevel, string message)
+            {
+
+                switch (logLevel)
+                {
+                    case LogLevel.Debug:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    case LogLevel.Info:
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    case LogLevel.Warning:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    case LogLevel.Error:
+                    case LogLevel.Fatal:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                }
+                Console.WriteLine($"{DateTime.Now}: { message }");
+                Console.ResetColor();
+            }
+
+        }
+        class Program
+        {
+
+            static void Main(string[] args)
+            {
+                ILogger consoleLogger = new ConsoleLogger();
+                consoleLogger.Bar();
+                consoleLogger.Foo();
+                consoleLogger.Log(LogLevel.Debug, "some event");
+                consoleLogger.Log(LogLevel.Warning, "some warning");
+                consoleLogger.Log(LogLevel.Fatal, "some fatal error");
+                consoleLogger.LogError("some error");
+            }
+        }
+    }
+}
---
+++ Interface/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Interface.DefaultInterfaceImplementation;
+
+namespace Interface
+{
+    internal static class Extensions
+    {
+        public static void Foo(this ILogger logger)
+        {
+            Console.WriteLine("Foo");
+        }
+    }
+}
